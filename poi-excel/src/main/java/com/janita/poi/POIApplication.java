b465b27e1bb032f7b5bbb1aee0b12b2ca3749fba
janita <zcj880902>
2017-06-12 10:37:23 +0800

    Initial commit

---
+++ poi-excel/src/main/java/com/janita/poi/POIApplication.java
@@ -0,0 +1,16 @@
+package com.janita.poi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Janita on 2017/6/12 0012- 上午 10:35
+ * 该类是：
+ */
+@SpringBootApplication
+public class POIApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(POIApplication.class, args);
+    }
+}
